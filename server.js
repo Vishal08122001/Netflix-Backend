8e2bbb89a7613a6cecde24d0a63560daddb82d77
Vishal <vc31142@gmail.com>
2023-05-29 20:03:36 +0530

    abc

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const connectDb = require("./Config/dbConnection");
-// const dotenv = require("dotenv").config();
-const PORT = 8080;
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 const userRoutes = require("./routes/UserRoutes");
 
-connectDb();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/user", userRoutes);
 
-app.listen(PORT, () => {
-  console.log("Running on " + PORT);
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log("Running on " + PORT);
+  });
 });
-app.use(cors());
